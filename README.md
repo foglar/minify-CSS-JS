dc8fe1b8899aaff53fc530d71a3dbac09922263f
foglar <kohout19fi@gymstola.cz>
2023-04-10 16:19:11 +0200

    README edit + SMALL changes

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Web-Minify
+# Minify CSS/JS
+
+![Czech][czechLangBadge]
+![Lines of code](https://img.shields.io/tokei/lines/github/foglar/minify-CSS-JS?style=for-the-badge)
+![GitHub issues](https://img.shields.io/github/issues/foglar/minify-CSS-JS?style=for-the-badge)
+![GitHub](https://img.shields.io/github/license/foglar/minify-CSS-JS?style=for-the-badge)
+
+Simple script to minify CSS and JS files.
+
+[![CodeFactor](https://www.codefactor.io/repository/github/foglar/minify-css-js/badge)](https://www.codefactor.io/repository/github/foglar/minify-css-js)
+![GitHub top language](https://img.shields.io/github/languages/top/foglar/minify-CSS-JS)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/foglar/minify-CSS-JS)
+![GitHub Repo stars](https://img.shields.io/github/stars/foglar/minify-CSS-JS?style=social)
+
+## Usage
+
+### Install
+
+```bash
+git clone https://github.com/foglar/minify-CSS-JS.git
+cd minify-CSS-JS
+pip3 install -r requirements.txt
+```
+
+### Run
+
+```bash
+python3 minify.py [files/folders...] [options...]
+```
+
+### Options
+
+Before options you should print files or folders, which you want to minify.
+It cannot scan folders recursively, so you should print all files and folders.
+If you select build mode, it will minify all files in the one file into the build folder (if you pick js and css files it sort them into **mini.css** and **mini.js**).
+
+```text
+[-s] [--silent] : Silent mode - don't show any messages and questions
+
+[-b] [--build] : Build mode - minify all files in the one file
+
+[-h] [--help] : Show help message
+
+[-v] [--version] : Show version
+
+[-l] [--license] : Show license
+```
+
+## License
+
+Minify CSS/JS is licensed under the MIT License - see the LICENSE file for details
+
+[czechLangBadge]: https://img.shields.io/badge/MADE%20IN-CZECH-red?style=for-the-badge
--- minify.py
+++ minify.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
-# CSS and JS minifier
+# CSS and JS minifier, by foglar
+# More information about it on https://www.github.com/foglar/minify-CSS-JS
 
 import sys, os
 import pyinputplus as pyip
@@ -8,7 +9,8 @@ import pyinputplus as pyip
 
 def main():
     """Main function"""
-    print("CSS and JS minifier")
+    print("CSS and JS minifier, by foglar")
+    print('More info at: https://www.github.com/foglar/minify-CSS-JS')
     print("-------------------")
 
     while True:
@@ -19,10 +21,17 @@ def main():
             # Check if path is to a folder or a file
             if os.path.exists(path_to_file):
                 if is_folder(path_to_file) == True:
-                    minify_folder(path_to_file, forced, onefile)
+                    try:
+                        minify_folder(path_to_file, forced, onefile)
+                    except KeyboardInterrupt:
+                        print("Files are not converted succesfully!")
+                        sys.exit()
                 else:
                     minify(path_to_file, forced, onefile)
                 break
+            else:
+                print("File not found!")
+                sys.argv.remove(sys.argv[1])
         # Check if multiple paths to files are provided
         elif len(sys.argv) > 2:
             for i in range(1, len(sys.argv)):
@@ -30,7 +39,11 @@ def main():
                 if os.path.exists(path_to_file):
                     if is_folder(path_to_file) == True:
                         print(f"Folder {os.path.abspath(path_to_file)}")
-                        minify_folder(path_to_file, forced, onefile)
+                        try:
+                            minify_folder(path_to_file, forced, onefile)
+                        except KeyboardInterrupt:
+                            print("Files are not converted succesfully!")
+                        sys.exit()
                     else:
                         print(f"File {os.path.abspath(path_to_file)}")
                         minify(path_to_file, forced, onefile)
@@ -40,19 +53,28 @@ def main():
             break
         # Ask for path to file
         else:
-            print("Please provide a path to a file")
-            path_to_file = pyip.inputFilepath("Path: ", mustExist=True)
+            while True:
+                print("Please provide a path to a file")
+                path_to_file = input("> ")
+                if os.path.exists(path_to_file):
+                    break
+                else:
+                    print("This file does not exist. Enter valid path.")
+                    print("")
+                    continue
             minify(path_to_file, forced, onefile)
+
             break
 
 
 def cmd_arguments():
+    """Set up arguments from command line"""
     forced = False
     build_to_one_file = False
 
     for i in range(len(sys.argv)):
         # Check if forced flag is provided
-        if sys.argv[i] == "-f" or sys.argv[i] == "--forced":
+        if sys.argv[i] == "-s" or sys.argv[i] == "--silent":
             forced = True
         # Check if build flag is provided
         elif sys.argv[i] == "-b" or sys.argv[i] == "--build":
@@ -66,7 +88,7 @@ def cmd_arguments():
             sys.exit()
         elif sys.argv[i] == "-h" or sys.argv[i] == "--help":
             print("Usage: minify.py [FILES]... [OPTION]...")
-            print("-f, --forced\t\tForce minify files")
+            print("-s, --silent\t\tSilently minify files")
             print("-b, --build\t\tBuild all files to one file")
             print("-v, --version\t\tPrint version")
             print("-l, --license\t\tPrint license")
@@ -110,7 +132,9 @@ def minify(path, is_forced, is_onefile):
                 try:
                     os.makedirs(folder[0] + "/build")
                 except PermissionError:
-                    print("Permission denied, please run as administrator or create build folder manually")
+                    print(
+                        "Permission denied, please run as administrator or create build folder manually"
+                    )
                     sys.exit()
             f = open(folder[0] + "/build/mini.css", "a")
             f.write(css)
@@ -126,7 +150,9 @@ def minify(path, is_forced, is_onefile):
                 try:
                     os.makedirs(folder[0] + "/build")
                 except PermissionError:
-                    print("Permission denied, please run as administrator or create build folder manually")
+                    print(
+                        "Permission denied, please run as administrator or create build folder manually"
+                    )
                     sys.exit()
             f = open(folder[0] + "/build/mini.css", "a")
             f.write(js)
@@ -171,28 +197,51 @@ def minify_folder(path, is_forced, is_onefile):
             print("File type not supported")
         print("-------------------")
 
+
 def minify_js(path):
     """Minify JS file"""
-    print(f"File size before: {os.path.getsize(path)} bites")
 
     with open(path, "r") as f:
         js = f.read()
 
+    # Remove comments
+    i = 0
+    while i < len(js):
+
+        if js[i - 1] == "/" and js[i] == "/":
+            start = i - 1
+            for j in range(i, len(js)):
+                if js[j] == "\n":
+                    end = j
+                    js = js.replace(js[start:end], "")
+                    i = 0
+                    break
+
+        i = i + 1
+
     js = js.replace("\n", "")
     js = js.replace("\t", "")
+
+    i = 0
+    while i < len(js):
+        if js[i - 1] == " " and js[i] == " ":
+            js = js[: i - 1] + js[i + 1 :]
+            i = 0
+
+        i = i + 1
 
     with open("minified.txt", "w") as f:
         f.write(js)
 
-    print(f'File size after: {os.path.getsize("minified.txt")} bites')
+    size_before = os.path.getsize(path)
+    size_after = os.path.getsize("minified.txt")
+
+    space_saved(size_before, size_after)
     return js
 
 
 def minify_css(path):
     """Minify CSS file"""
-
-    size_before = os.path.getsize(path)
-    print(f"File size before: {size_before} bites")
 
     with open(path, "r") as f:
         css = f.read()
@@ -227,12 +276,11 @@ def minify_css(path):
     with open("minified.txt", "w") as f:
         f.write(css)
 
+    size_before = os.path.getsize(path)
     size_after = os.path.getsize("minified.txt")
-    print(f'File size after: {size_after} bites')
 
-    percent = round((size_before - size_after) / size_before * 100)
-    print(f"Minified by {percent}%")
-    
+    space_saved(size_before, size_after)
+
     return css
 
 
@@ -247,6 +295,7 @@ def file_edit(path, confirm_question):
         os.rename("minified.txt", path)
         print(f"File succesfully minified!")
 
+
 def confirm_dialog():
     """Confirm action"""
     while True:
@@ -258,6 +307,7 @@ def confirm_dialog():
         else:
             print("Please enter y or n")
 
+
 def is_folder(path):
     """Check if path is a file or a folder"""
     if os.path.isfile(path) == True:
@@ -267,7 +317,22 @@ def is_folder(path):
     else:
         return None
 
+
+def space_saved(size_before, size_after):
+    print(f"File size before: {size_before} bites")
+    print(f"File size after: {size_after} bites")
+    print("")
+    percent_Saved = round((size_before - size_after) / size_before * 100)
+    print(f"You save up {percent_Saved}% of size of the file!")
+
+    bits_Saved = size_before - size_after
+    print(f"File is about {bits_Saved} bits smaller now!")
+
+    print("")
+    return percent_Saved, bits_Saved
+
+
 if __name__ == "__main__":
     main()
-    if os.path.exists('./minified.txt') == True:
+    if os.path.exists("./minified.txt") == True:
         os.remove("./minified.txt")
